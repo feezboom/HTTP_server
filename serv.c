b9ab86485fde9b1bc134f462c806240d2411b26a
Algebroid <algebroid.2011@gmail.com>
2015-05-03 13:15:07 +0500

    a bit more

--- serv.c
+++ serv.c
@@ -1,3 +1,21 @@
+/*
+ * Логика
+ * -Сначала мы запускаем наш сервер для готовности приема входящих соединений(listen)
+ * 
+ * -После этого мы сразу же запускаем столько потоков, 
+ * сколько нам нужно максимум одновременно обрабатывающихся клиентов.(run_threads)
+ * Здесь же мы создаем неименованый канал, чтобы класть туда текущие,
+ * неудовлетворённые запросы.
+ * 
+ * -Далее мы ждем входящее соединение (accept)
+ * 
+ * -Как только дождались кого-нибудь, кладем наш запрос в созданный нами ранее пайп.
+ * 
+ * -Потоки же в это время следят за этим пайпом, готовые прочитать запрос из него,
+ * как только он там появится, и удовлетворить его.
+ * */
+
+
 #include <pthread.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -14,6 +32,7 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define THREADS_NUMBER 10
 
 #define PORT 80
 #define MAX_REQUEST_SIZE 1024
@@ -24,12 +43,12 @@
 #define BAD_REQUEST 400
 #define NOT_FOUND 404
 
-void run_server(char* server_dir, int server_port);
+int run_server(char* server_dir, int server_port);
 int run_handling_requests(int server_fd);
 int parse(char* request, char* path);
 int respond(char* request);
 int perform_request(int client, char* request);
-void* client_processing(void *client_number);
+void* thread_processing(void *client_number);
 
 
 
@@ -37,8 +56,26 @@ void* client_processing(void *client_number);
  * Вместо version 1.0, or 1,1
  * */
  
- int perform_request(int client, char* request)
- {
+int* run_threads(int threads_number) //Пусть возвращает пайп для обмена с потоками данными...
+{
+	assert(threads_number > 0);
+	 
+	int i; int* our_pipe = (int*)malloc(2*sizeof(int));
+	pipe(our_pipe);						//Создаём наш канал для очереди запросов
+	
+	pthread_t thread_id;				//Если создание потока успешно:
+		
+	for (i = 0; i < threads_number; ++i)
+		pthread_create(&thread_id, NULL, thread_processing, (void*)our_pipe);
+		
+	/*pthread_create(Идентификатор потока, атрибуты, функция запуска, аргумент функции запуска)*/
+	
+	return our_pipe;
+ }
+ 
+ 
+int perform_request(int client, char* request)
+{
 	 /*
 	  * 1) Отдача статики, т.е., как я понял, запрашивается файл, конкретный
 	  * 2) Поддержка скриптов через cgi - запуск скрипта?????
@@ -51,50 +88,42 @@ void* client_processing(void *client_number);
 	 return 0;
  }
  
- 
- 
- void* client_processing(void *client_number)
-{
-/*Этот кусок кода должен быть в отдельном потоке реализован*/		
-/*		char buffer[MAX_REQUEST_SIZE], *request;
-		ssize_t received_bytes = recv(client, buffer, MAX_REQUEST_SIZE, 0);
-		assert(received_bytes >=0);//Проверка с отловом ошибки, если нужно
-		buffer[received_bytes] = '\0';
-		request = buffer;	 
-		perform_request(client, request);
-*/
-/*Этот кусок кода должен быть в отдельном потоке реализован*/
-	return client_number;
+/*MUTEX!*/
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+void* thread_processing(void *our_pipe)
+{		
+	//Здесь должно быть описано ожидание потоком момента, пока в пайп
+	//не будут записаны данные
+		 
+//		perform_request(client, request);
+
+	return our_pipe;
 }
- int run_handling_requests(int server_fd)//Функция обработки запросов
- {
-	 int clients[2]; clients[0] = 0;
-//clients[0] - Количество клиентов, обрабатывающихся в данный момент
-//clients[1] - Идентификатор текущего клиента (дескриптор сокета)
-	 while (0 == 0)
-	 {
-		 //если, вдруг, клиентов слишком много, то добавить в очередь
-		 //в очереди ждем, пока clients[0] не станет подходящей, чтобы начать обработку нового клиента
+ 
+ 
+int run_handling_requests(int server_fd)	//Функция обработки запросов
+{
+	int* our_pipe = run_threads(THREADS_NUMBER);
+	while (0 == 0)
+	{
 		int client = accept(server_fd, NULL, 0);
 		assert(client >=0);					//Как бы проверка на ошибку accept
-		clients[0]++;						//Присоединился один клиент
-		//И здесь мы говорим: "А давайте засунем обработку этого клиента в отдельный поток"
+											//На этом этапе к нам поступил очередной запрос...
+		char buffer[MAX_REQUEST_SIZE], *request;
+		ssize_t received_bytes = recv(client, buffer, MAX_REQUEST_SIZE, 0);
+		assert(received_bytes >=0);			//Проверка с отловом ошибки, если нужно
+		buffer[received_bytes] = '\0'; 		//Запрос без символа-терминатора строки...
+		request = buffer;
 		
-		pthread_t thread_id;				//Если создание потока успешно:
-		pthread_mutex_t mtx;
-		pthread_cond_t decreased;
-		pthread_create(&thread_id, NULL, client_processing, (void*)clients);	
-		/*pthread_create(Идентификатор потока, атрибуты, функция запуска, аргумент функции запуска)*/	
-	 }
-	 return 0;
+											//Уже обработанный мы кладём в неименованый канал(пайп).
+		write(our_pipe[1], request, received_bytes + 1);
+//Далее все заботы уже на потоках, здесь пока вроде всё...
+	}
+	return 0;
  }
 
 
-
-
-
-
-void run_server(char* server_dir, int server_port)
+int run_server(char* server_dir, int server_port)
 {
 	int server;
 	struct sockaddr_in sa;
@@ -113,9 +142,8 @@ void run_server(char* server_dir, int server_port)
 	bind(server, (struct sockaddr *)&sa, sizeof(sa));		//Binding - assotiation server_socket_fd with sa?
 	listen(server, 10);										//listening with maximum 10 requests in queue
 //Preparing server finished, server is listening...
+	return server;
 }
-
-
 
 
 int main(int argc, char** argv)
@@ -124,10 +152,13 @@ int main(int argc, char** argv)
 		exit(EXIT_FAILURE);
 	
 	char *current_dir = argv[1];
+	int server_fd;
 //	int port = atoi(argv[2]);
 	
-	run_server(current_dir, PORT);
-	//Here our sever is ready to receive requests...
+	server_fd = run_server(current_dir, PORT);	
+//Теперь сервер готов к приёму входящих соединений
+	run_handling_requests(server_fd);
+//Запускаем обработку входящих соединений сервера server_fd
 	
 	
 	
